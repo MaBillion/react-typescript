e1216eaaa9727458b708cd5cb86b490c047d29a6
chao.ma <chao.ma@brilliantaero.com>
2018-11-27 12:15:14 +0800

    add router

---
+++ src/screen/MessageCenter.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class MessageCenter extends React.Component {
+  public render() {
+    return (
+      <div className="MessageCenter">
+        MessageCenter
+      </div>
+    );
+  }
+}
+
+export default MessageCenter;
