9e39a6af85ec112fb9f4bc151e10f9d5a44fb6dd
chao.ma <chao.ma@brilliantaero.com>
2018-12-10 16:58:56 +0800

    增加计划列表

--- src/screen/router.tsx
+++ src/screen/router.tsx
@@ -3,16 +3,18 @@ import { Route, Redirect } from 'react-router-dom';
 
 import Home from './Home';
 import MessageCenter from './MessageCenter';
-import Login from './Login'
+import Login from './Login';
+import PlanList from './PlanList';
 
 export default class extends React.Component{
     public render() {
         return (
-            <div>
+            <div style={{height: '100%', width: '100%'}}>
                 <Route exact={true} path='/' render={()=><Redirect to='/Home' />} />
                 <Route path='/Home' component={Home} />
                 <Route path='/MessageCenter' component={MessageCenter} />
                 <Route path='/Login' component={Login} />
+                <Route path='/PlanList' component={PlanList} />
             </div>
         )
     }
