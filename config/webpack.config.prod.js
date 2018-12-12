53309aaac8cb37769ac6fcb13bdd61cd867cba71
chao.ma <chao.ma@brilliantaero.com>
2018-12-12 11:56:23 +0800

    去掉注释，调整缩进

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -190,21 +190,21 @@ module.exports = {
         new UglifyJsPlugin({
             uglifyOptions: {
                 parse: {
-                ecma: 8,
+                    ecma: 8,
                 },
                 compress: {
-                ecma: 5,
-                warnings: false,
-                comparisons: false,
-                inline: 1,
+                    ecma: 5,
+                    warnings: false,
+                    comparisons: false,
+                    inline: 1,
                 },
                 mangle: {
-                safari10: true,
+                    safari10: true,
                 },
                 output: {
-                ecma: 5,
-                comments: false,
-                ascii_only: true,
+                    ecma: 5,
+                    comments: false,
+                    ascii_only: true,
                 },
             },
             parallel: true,
