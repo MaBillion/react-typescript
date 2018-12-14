171956778e9e5ec86a1fc29bdcad3d724290d0de
chao.ma <chao.ma@brilliantaero.com>
2018-12-14 11:32:00 +0800

    计划详情完善类型

--- src/store/PlanDetail.ts
+++ src/store/PlanDetail.ts
@@ -7,14 +7,63 @@ export interface TakeoffLandType extends LatLngAddrLocal {
     type: 1 | 2 | 3
 }
 
+export interface RepresentedCompanysType {
+    company_id: number,
+    abbr_name: string
+}
+
+export interface UserAircraftsType {
+    user_aircraft_id: number,
+    serial_num: string,
+    model: string,
+    aircraft_type: string,
+    manufacturer: string
+}
+
+export interface OperatorsType {
+    operator_id: number,
+    name_zh: string,
+    id_type: number,
+    id_code: string,
+    mobile: string
+}
+
+export interface NotificationAreasType {
+    area_name: string,
+    area_id: number
+}
+
+export interface ApprovalType {
+    approval_id: number,
+    approval_name: string,
+    space_infos: AirspaceServer
+}
+
+export interface ApprovalInfosType {
+    approval_id: number,
+    approval_name: string,
+    serial_num: string,
+    status: number
+}
+
+export interface PlanRequestTrackingType {
+    status: number,
+    time: string,
+    operator: {
+        id: number,
+        name: string,
+        mobile: string
+    }
+}
+
 export interface PlanDetailType extends PlanListType {
     short_company_name: string,
     short_represented_company_name: string,
-    represented_companys: object[],
+    represented_companys: RepresentedCompanysType[],
     plan_type_desc: string,
-    user_aircrafts: object[],
+    user_aircrafts: UserAircraftsType[],
     notes: string,
-    operators: object[],
+    operators: OperatorsType[],
     end_time: string,
     release_time: {
         start_time: string,
@@ -29,21 +78,18 @@ export interface PlanDetailType extends PlanListType {
         end_time: string,
         sortie: number
     },
-    notification_areas: object[],
-    accept_area: {
-        area_name: string,
-        area_id: number
-    },
+    notification_areas: NotificationAreasType[],
+    accept_area: NotificationAreasType,
     new_airspaces: {
         tmp: AirspaceServer[],
-        approval: object[]
+        approval: ApprovalType[]
     },
     backup_airports: LatLngAddrLocal[],
     approve_notes: string,
     flight_rules: 1 | 2 | 3,
-    approval_infos: object[],
+    approval_infos: ApprovalInfosType[],
     takeoff_landing_points : TakeoffLandType[],
-    plan_request_tracking: object[],
+    plan_request_tracking: PlanRequestTrackingType[],
     company_id: number
 }
 
